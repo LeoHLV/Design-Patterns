fcf96df7b2d9d828bbec012c23cc93e1025d8708
Leonardo Henrique Lopes Vieira <lhlv.dev@gmail.com>
2022-10-06 19:22:43 -0300

    Delete 3_Behavioral directory

--- 3_Behavioral/1_Chain of Responsibility/Strategy/ComPadrao/IEnviaMensagem.cs
+++
@@ -1,9 +0,0 @@
-namespace PadraoChainOfResponsibility.Strategy.ComPadrao
-{
-    public interface IEnviaMensagem
-    {
-        public void Enviar(List<Mensagem> mensagens);
-        IEnviaMensagem CriarEnvio(string tipo);
-        IEnviaMensagem ProximoEnvio { get; set; }
-    }
-}
--- 3_Behavioral/7_Observer/Observers/IAcaoAposCadastroPessoa.cs
+++
@@ -1,7 +0,0 @@
-namespace EstudoCS.Observers
-{
-    public interface IAcaoAposCadastroPessoa
-    {
-        void Executar(Pessoa pessoa);
-    }
-}
